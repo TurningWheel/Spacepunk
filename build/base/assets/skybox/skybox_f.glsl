7ff0d6315e4a7ea57e592a76f6d840b71f638130
SheridanR <sheridan.rathbun@gmail.com>
2019-11-23 16:06:10 -0800

    Fix over-exposed skybox
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- build/base/assets/skybox/skybox_f.glsl
+++ build/base/assets/skybox/skybox_f.glsl
@@ -6,7 +6,8 @@ in vec3 WorldPos;
 in vec4 Colors;
 in vec3 Tangent;
 
-out vec4 FragColor;
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 FragColorBright;
 
 uniform vec3 gCameraPos;
 uniform samplerCube gCubemap;
@@ -16,4 +17,12 @@ void main() {
 	vec3 lWorldPos   = WorldPos;
 	vec3 lCameraDir  = normalize(gCameraPos - lWorldPos);
 	FragColor = texture(gCubemap, lCameraDir * -1.f);
+	
+	// check whether fragment output is higher than threshold, if so output to bloom buffer
+    float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+    if (brightness > 1.0) {
+        FragColorBright = vec4(FragColor.rgb, 1.f);
+    } else {
+        FragColorBright = vec4(0.f, 0.f, 0.f, 1.f);
+    }
 }
