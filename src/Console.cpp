4fbd38888c25b1435bc199f71c6fd2c24be43c21
SheridanR <sheridan.rathbun@gmail.com>
2020-04-04 11:28:05 -0700

    remove some static initializer crap in Console.cpp
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- src/Console.cpp
+++ src/Console.cpp
@@ -2,27 +2,16 @@
 
 #include "Console.hpp"
 
-#ifdef PLATFORM_WINDOWS
-#ifdef _MSC_VER
-#pragma warning(disable: 4073) // initializers put in library initialization area
-#endif
-#pragma init_seg(lib)
-static Map<String, Cvar*> cvars;
-static Map<String, Ccmd*> ccmds;
-#else
-static Map<String, Cvar*> cvars __attribute__((init_priority(101)));
-static Map<String, Ccmd*> ccmds __attribute__((init_priority(102)));
-#endif
-
 Cvar::Cvar(const char* _name, const char* _desc, const char* _value) {
 	name = _name;
 	desc = _desc;
 	value = _value;
 	defaultValue = _value;
-	cvars.insert(_name, this);
+	getMap().insert(_name, this);
 }
 
 Map<String, Cvar*>& Cvar::getMap() {
+	static Map<String, Cvar*> cvars;
 	return cvars;
 }
 
@@ -30,9 +19,10 @@ Ccmd::Ccmd(const char* _name, const char* _desc, int(*_func)(int, const char**))
 	name = _name;
 	desc = _desc;
 	func = _func;
-	ccmds.insert(_name, this);
+	getMap().insert(_name, this);
 }
 
 Map<String, Ccmd*>& Ccmd::getMap() {
+	static Map<String, Ccmd*> ccmds;
 	return ccmds;
 }
